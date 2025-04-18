7f32192bb16beb3175f7d3fd68e161077045afde
Mansi Vaghasiya <vaghasiyamansi999@gmail.com>
2025-04-18 23:26:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 let imgBox = document.getElementById("imgBox");
 let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
-function generateQR(){
-    if(qrText.value.length > 0){
-        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+
+function generateQR() {
+    if (qrText.value.length > 0) {
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(qrText.value);
         imgBox.classList.add('show-img');
-    }
-    else{
+    } else {
         qrText.classList.add('error');
-        setTimeout(()=>{
-            qrText.classList.add('error');            
-        },1000);
+        setTimeout(() => {
+            qrText.classList.remove('error');
+        }, 1000);
     }
-    }
-    
+}
